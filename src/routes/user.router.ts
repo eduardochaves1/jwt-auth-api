12ddb2bfc5692d8eff3e2e3974cfc2de4e462850
Eduardo Chaves <henriqueduardo2002@gmail.com>
2025-01-05 16:39:54 -0300

    Add user routes for CRUD operations and authentication

---
+++ src/routes/user.router.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import {
+  createUser,
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  loginUser,
+  logoutUser,
+  promoteAdmin,
+  demoteAdmin,
+} from '../controllers/user.controller'
+
+const router = Router();
+
+router.get('/', getUsers);
+router.post('/', createUser);
+
+router.get('/:username', getUser);
+router.put('/:username', updateUser);
+router.delete('/:username', deleteUser);
+
+router.post('/:username/login', loginUser);
+router.delete('/:username/logout', logoutUser);
+
+router.post('/:username/promote', promoteAdmin);
+router.delete('/:username/demote', demoteAdmin);
+
+export default router;
