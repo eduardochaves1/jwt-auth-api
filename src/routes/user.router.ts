7bf839cf805a1406e6decb7b7484655a24a5ec97
Eduardo Chaves <henriqueduardo2002@gmail.com>
2025-01-06 20:29:00 -0300

    Add username parameter validation to user routes

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { zUser } from "../models/user.model";
-import validateRequest from "../middlewares/validateRequest";
+import { zUser, zUsername } from "../models/user.model";
+import validateRequest, { validateParam } from "../middlewares/validateRequest";
 import {
   createUser,
   updateUser,
@@ -13,19 +13,21 @@ import {
   demoteAdmin,
 } from '../controllers/user.controller'
 
+const validateUsernameParam = validateParam('username', zUsername);
+
 const router = Router();
 
 router.get('/', getUsers);
 router.post('/', validateRequest(zUser), createUser);
 
-router.get('/:username', getUser);
-router.put('/:username', validateRequest(zUser), updateUser);
-router.delete('/:username', deleteUser);
+router.get('/:username', validateUsernameParam, getUser);
+router.put('/:username', validateUsernameParam, validateRequest(zUser), updateUser);
+router.delete('/:username', validateUsernameParam, deleteUser);
 
-router.post('/:username/login', loginUser);
-router.delete('/:username/logout', logoutUser);
+router.post('/:username/login', validateUsernameParam, loginUser);
+router.delete('/:username/logout', validateUsernameParam, logoutUser);
 
-router.post('/:username/promote', promoteAdmin);
-router.delete('/:username/demote', demoteAdmin);
+router.post('/:username/promote', validateUsernameParam, promoteAdmin);
+router.delete('/:username/demote', validateUsernameParam, demoteAdmin);
 
 export default router;
