35a6f6d4d8ecea222f2fe91c8165a2a839838cc5
Eduardo Chaves <henriqueduardo2002@gmail.com>
2025-01-07 19:41:21 -0300

    Add MongoDB connection utility

---
+++ src/db.connect.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const dbUsername = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbDomain = process.env.MONGODB_DOMAIN;
+const dbName = process.env.MONGODB_DATABASE;
+
+const mongodbConnection = async () => {
+  try {
+    await mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@${dbDomain}/${dbName}`)
+    return console.log('Successfully Connected to MongoDB at ' + dbName)
+  } catch (error) {
+    return console.error('Error While Connecting MongoDB: ', error)
+  }
+}
+
+export default mongodbConnection;
