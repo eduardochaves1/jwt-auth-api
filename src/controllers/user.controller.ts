d3d392b164a266f3776d6a05f5e43173aa5ab18c
Eduardo Chaves <henriqueduardo2002@gmail.com>
2025-01-11 17:40:24 -0300

    Implement delete user functionality with appropriate response handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -40,7 +40,17 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
 
 export const deleteUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    res.status(404).json({ message: "Endpoint to be developed" })
+    const { username } = req.params;
+
+    const deletedUser = await User.deleteOne({ username });
+
+    if (!deletedUser.acknowledged) {
+      res.status(500).json({ error: "The delete operation was not acknowledged by the database"})
+    } else if (deletedUser.deletedCount < 1) {
+      res.status(404).json({ error: `No user found with the username ${username}`})
+    } else {
+      res.status(200).json(deletedUser);
+    }
   } catch (error) {
     res.status(500).json({
       message: 'Internal Server Error',
