32a08b1eacf693bf3547dbe31d11a6569f8f44e8
Eduardo Chaves <henriqueduardo2002@gmail.com>
2025-01-13 22:53:41 -0300

    ADD: integrate jwtAuth middleware, enabling authentication handling

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv-safe';
 import mongodbConnection from './db.connect';
 import errorHandler from './middlewares/errorHandler';
 import userRouter from './routes/user.router';
+import jwtAuth from './middlewares/jwtAuth';
 
 const startServer = async () => {
   dotenv.config();
@@ -13,6 +14,7 @@ const startServer = async () => {
 
   app
     .use(express.json())
+    .use(jwtAuth)
     .use('/api/users', userRouter)
     .use(errorHandler)
     .listen(port, () => console.log(`Server is running on port ${port}`));
